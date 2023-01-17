53ad275f2afd94d57eebc6cf0bc055d31500268b
Ridvan Yel <yelridvan72@gmail.com>
2023-01-17 01:59:27 +0100

    proje6

---
+++ proje6/src/proje6/ÜsAlma.java
@@ -0,0 +1,26 @@
+package proje6;
+
+import java.util.Scanner;
+
+public class ÜsAlma {
+
+	public static void main(String[] args) {
+		
+		int sayi =100 , toplam = 0;
+		
+		for (int i =1 ; i<=sayi ; i++) {
+			
+			
+			toplam+=i;
+			
+		}
+		System.out.println("Toplam= "+ toplam);
+		
+		
+		
+				
+				
+		
+		}
+
+}
